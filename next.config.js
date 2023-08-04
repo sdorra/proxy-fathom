ba120e704b59f2ccf9d33d332279e22c350fcfe0
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2023-08-04 13:51:36 +0200

    Fixed fathom rewrites

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => [
-    {
-      source: "/script.js",
-      destination: "https://cdn.usefathom.com/script.js",
-    },
-    {
-      source: "/",
-      destination: "https://cdn.usefathom.com/",
-      has: [
-        {
-          type: "query",
-          key: "sid",
-          value: "IVBPNICR",
-        },
-      ],
-    },
-  ],
+  rewrites: async () => ({
+    beforeFiles: [
+      {
+        source: "/script.js",
+        destination: "https://cdn.usefathom.com/script.js",
+      },
+      {
+        source: "/",
+        destination: "https://www.heise.de/",
+        has: [
+          {
+            type: "query",
+            key: "sid",
+            value: "IVBPNICR",
+          },
+        ],
+      },
+    ],
+  }),
 };
 
 module.exports = nextConfig;
